f23455ce3e8d559fa4908a482f15068cf72d0897
fabricio-pedro <wolfxzero2@gmail.com>
2025-04-19 22:48:12 -0300

    implementaçao de hook useSummary

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 
 
 export function App() {
@@ -12,7 +13,9 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       
+      <TransactionsProvider>
       <Transactions/>
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 
 
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  price: number;
-  category: string;
-  createdAt: string;
-}
 
 
 export function Transactions() {
 
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
  
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, []);
+  const { transactions } = useContext(TransactionsContext)
+ 
 
 
   return (
@@ -48,14 +33,14 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight $variant={transaction.type}>
-                      {new Intl.NumberFormat('pt-BR', {
-                        style: 'currency',
-                        currency: 'BRL'
-                      }).format(transaction.price)}
+                      {transaction.type === 'outcome' &&  <span>-   </span>}
+
+                    
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               ) 
             })}
